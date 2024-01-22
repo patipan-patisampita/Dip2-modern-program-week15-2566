ca091f857112f8e1ce0bc31758e7dd2471e658fb
patipan-patisampita <patipan-patisampita@gmail.com>
2024-01-22 16:07:36 +0700

    update

--- lib/widgets/list_view_menu.dart
+++ lib/widgets/list_view_menu.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
+class ListMenu {
+  final String? title;
+  final String? subtitle;
+  final IconData? icon;
+  const ListMenu({this.title, this.subtitle, this.icon});
+}
+
+List<ListMenu> menus = [
+  const ListMenu(title: 'MENU-1', subtitle: 'SUB-1', icon: Icons.person),
+  const ListMenu(title: 'MENU-2', subtitle: 'SUB-2', icon: Icons.person_add),
+  const ListMenu(title: 'MENU-3', subtitle: 'SUB-3', icon: Icons.wifi),
+  const ListMenu(title: 'MENU-4', subtitle: 'SUB-4', icon: Icons.build),
+];
+
 class ListViewMenu extends StatelessWidget {
   const ListViewMenu({super.key});
 
@@ -14,19 +28,19 @@ class ListViewMenu extends StatelessWidget {
         child: ListView.separated(
           itemBuilder: (context, index) {
             return ListTile(
-              title: Text("MENU-1"),
-              subtitle: Text("SUB-1"),
-              leading: CircleAvatar(child: Icon(Icons.person)),
+              title: Text("${menus[index].title}"),
+              subtitle: Text("${menus[index].subtitle}"),
+              leading: CircleAvatar(child: Icon(menus[index].icon)),
               trailing: Icon(Icons.arrow_right),
               onTap: () {
-                debugPrint("MENU-1");
+                debugPrint("${menus[index].title}");
               },
             );
           },
           separatorBuilder: (context, index) {
             return const Divider(color: Colors.red);
           },
-          itemCount: 4,
+          itemCount: menus.length,
         ),
       ),
     );
